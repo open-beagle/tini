86fd61df5611547a55400623d702f20e0fe8fbb0
shucheng <shucheng@bd-apaas.com>
2023-04-20 23:08:48 +0800

    v0.19.0 support loong64

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -17,8 +17,7 @@ elif [ "$LOCAL_ARCH" = "aarch64" ]; then
 elif [ "$LOCAL_ARCH" = "loongarch64" ]; then
   TARGET_ARCH="loong64"
 elif [ "$(echo $LOCAL_ARCH | head -c 5)" = "ppc64" ]; then
-  # TARGET_ARCH="ppc64le"
-  TARGET_ARCH="unsupported"
+  TARGET_ARCH="ppc64le"
 elif [ "$(echo $LOCAL_ARCH | head -c 6)" = "mips64" ]; then
   TARGET_ARCH="mips64le"
 else
